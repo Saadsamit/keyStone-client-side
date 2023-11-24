7359fbdb44eaf0822a8dc86a3e1b3f3cbef97189
MD SAMIT INTESAR <saadsamit64@gmail.com>
2023-11-25 00:44:09 +0600

    all properties page working

---
+++ src/components/PropertiesCard.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const PropertiesCard = ({ data }) => {
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure>
+        <img src={data.property.image} alt="Shoes" className="w-full" />
+      </figure>
+      <div className="card-body">
+      <div className="avatar cursor-pointer flex items-center gap-2">
+          <div className="w-8 rounded-full">
+            <img src={ data.agent.image} />
+          </div>
+          <p>{data.agent.name}</p>
+        </div>
+        <h2 className="card-title">
+          {data.property.title}
+          <div className="badge bg-[#1F8A70] text-[#D7FBE8]">{data.status}</div>
+        </h2>
+        <p>{data.property.location}</p>
+        <p>{data.property.PriceRange}</p>
+        <div className="card-actions justify-end">
+          <Link className="btn bg-[#1F8A70] text-[#D7FBE8] hover:bg-[#00425A]">see details</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+PropertiesCard.propTypes = {
+  data: PropTypes.object,
+};
+
+export default PropertiesCard;
--- src/pages/AllProperties.jsx
+++ src/pages/AllProperties.jsx
@@ -1,9 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PropertiesCard from "../components/PropertiesCard";
+import Container from "../components/Container";
 
 const AllProperties = () => {
+    const [allData, setallData] = useState([])
+    useEffect(()=>{
+        axios('/public/propety.json')
+    .then(res=>{
+        setallData(res.data);
+    })
+    },[])
     return (
-        <div>
-            properties
-        </div>
+        <Container data={'py-20'}>
+           <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+              {
+                allData.map((data,idx)=><PropertiesCard key={idx} data={data}/>)
+              }
+           </div>
+        </Container>
     );
 };
 
